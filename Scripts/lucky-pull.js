c59145f5bcb3357676ba84a4deaddfe85d5bc7dc
Nunners <97916154+Nunnerrs@users.noreply.github.com>
2023-01-08 15:08:21 -0800

    fixed display issues

--- Scripts/lucky-pull.js
+++ Scripts/lucky-pull.js
@@ -7,204 +7,26 @@ var continueButton = document.getElementById("continueButton");
 var colorBlindHelp = document.getElementById("colorBlindHelp");
 var colorBlindOn = false;
 var largePullAmount = 10;
-var oneStars = false;
-var twoStars = false;
-var fiveStars = true;
+//var oneStars = false;
+//var twoStars = false;
+//var fiveStars = true;
 var oneStarChance = 0;
 var twoStarChance = 0;
 var threeStarChance = 85;
 var fourStarChance = 13;
 var fiveStarChance = 2;
 var max = 100;
+var mode = 0;
 
 const rewards = [];
-
-/*function changeType() {
-    let type = gachaType.value;
-    if (type == 1) {
-        largePull = 10;
-        oneStars = false;
-        twoStars = false;
-        fiveStars = true;
-        oneStarChance = 0;
-        twoStarChance = 0;
-        threeStarChance = 85;
-        fourStarChance = 14;
-        fiveStarChance = 1;
-        max = 100;
-        alert("set to rpg");
-    } else if (type == 2) {
-        largePull = 10;
-        oneStars = false;
-        twoStars = true;
-        fiveStars = false;
-        oneStarChance = 0;
-        twoStarChance = 885;
-        threeStarChance = 85;
-        fourStarChance = 30;
-        fiveStarChance = 0;
-        max = 1000;
-        alert("set to band");
-    } else if (type == 3) {
-        largePull = 1;
-        oneStars = true;
-        twoStars = true;
-        fiveStars = true;
-        oneStarChance = 20;
-        twoStarChance = 35;
-        threeStarChance = 27;
-        fourStarChance = 15;
-        fiveStarChance = 3;
-        max = 100;
-        alert("set to adopt");
-    } else if (type == 4) {
-        largePull = 3;
-        oneStars = true;
-        twoStars = true;
-        fiveStars = true;
-        oneStarChance = 394;
-        twoStarChance = 265;
-        threeStarChance = 200;
-        fourStarChance = 140;
-        fiveStarChance = 1;
-        max = 1000;
-        alert("set to bubblegum");
-    };
-};*/
-
-/*function doPull() {
-    alert("1 pull…");
-    onePull.style = "visibility: hidden;";
-    largePull.style = "visibility: hidden;";
-    message.style = "visibility: visible;";
-    continueButton.style = "visibility: visible;";
-
-    let pull = Math.floor(Math.random() * max) + 1;
-
-    if (oneStars == true && pull <= oneStarChance) {
-        rewards.push("1");
-    };
-    if (oneStars == true && twoStars == true && pull > oneStarChance && pull <= twoStarChance) {
-        rewards.push("2");
-    };
-    if (oneStars == false && twoStars == true && pull <= twoStarChance) {
-        rewards.push("2");
-    };
-    if (twoStars == true && pull > twoStarChance && pull <= threeStarChance) {
-        rewards.push("3");
-    };
-    if (twoStars == false && gatchaType.value == "1" && pull <= threeStarChance) {
-        rewards.push("3");
-    };
-    if (pull > threeStarChance && pull <= fourStarChance) {
-        rewards.push("4");
-    };
-    if (fiveStars == true && pull > fourStarChance && pull <= fiveStarChance) {
-        rewards.push("5");
-    };
-    alert("u pulled a " + rewards[rewards.length - 1] + "-star!!");
-};*/
-
-/*function doLargePull() {
-    alert(largePullAmount + " pull…");
-    message.style = "visibility: visible;";
-    continueButton.style = "visibility: visible;";
-    let highest = 3;
-
-    for (let i = 1; i <= largePullAmount; i++) {
-        alert("1 pull…");
-        let pull = Math.floor(Math.random() * max) + 1;
-
-        if (oneStars == true && pull <= oneStarChance) {
-            rewards.push("1");
-            continue;
-        };
-        if (oneStars == true && twoStars == true && pull > oneStarChance && pull <= twoStarChance) {
-            rewards.push("2");
-            continue;
-        };
-        if (oneStars == false && twoStars == true && pull <= twoStarChance) {
-            rewards.push("2");
-            continue;
-        };
-        if (twoStars == true && pull > twoStarChance && pull <= threeStarChance) {
-            rewards.push("3");
-            continue;
-        };
-        if (twoStars == false && gatchaType.value == "1" && pull <= threeStarChance) {
-            rewards.push("3");
-            continue;
-        };
-        if (pull > threeStarChance && pull <= fourStarChance) {
-            rewards.push("4");
-            if (gachaType.value == "1") {
-                if (highest < 4) {
-                    highest = 4;
-                };
-            };
-            continue;
-        };
-        if (fiveStars == true && pull > fourStarChance && pull <= fiveStarChance) {
-            rewards.push("5");
-            if (gachaType.value == "1") {
-                if (highest < 5) {
-                    highest = 5;
-                };
-            };
-            continue;
-        };
-        alert("u pulled a " + rewards[rewards.length - 1] + "-star!!");
-    };
-    
-    if (highest == 4) {
-        continueButton.style = "color: rgb(200, 150, 255); visibility: visible;"
-    };
-    if (highest == 5) {
-        continueButton.style = "color: rgb(255, 175, 100); visibility: visible;"
-    };
-    if (gatchaType.value == "1") {
-        message.innerHTML = "Click to continue";
-        continueButton.innerHTML = "★";
-    };
-    if (gatchaType.value == "2") {
-        message.innerHTML = "Click to cut";
-        continueButton.innerHTML = "🎫";
-    };
-    if (gatchaType.value == "3") {
-        message.innerHTML = "Click to hatch";
-        continueButton.innerHTML = "🥚";
-    };
-    alert("completed execution");
-};*/
-
-/*function reward() {
-    message.innerHTML = "you pulled: " + rewards.toString() + ". good job!!!";
-    onePull.style = "visibility: visible;";
-    largePull.style = "visibility: visible;";
-    message.style = "visibility: hidden;";
-    continueButton.style = "visibility: hidden;";
-    rewards.length = 0;
-    alert("awards are displayed!");
-};*/
-
-/*function colorBlind() {
-    if (colorBlindOn == true) {
-        document.html.class = "colorBlind";
-        colorBlindOn = false;
-    } else {
-        document.html.class = "";
-        colorBlindOn = true;
-    };
-    alert("set color blind mode to " + colorBlindOn);
-};*/
 
 submit.addEventListener("click", function() {
     let type = gachaType.value;
     if (type == 1) {
         largePull = 10;
-        oneStars = false;
-        twoStars = false;
-        fiveStars = true;
+        //oneStars = false;
+        //twoStars = false;
+        //fiveStars = true;
         oneStarChance = 0;
         twoStarChance = 0;
         threeStarChance = 85;
@@ -214,9 +36,9 @@ submit.addEventListener("click", function() {
         alert("set to rpg");
     } else if (type == 2) {
         largePull = 10;
-        oneStars = false;
-        twoStars = true;
-        fiveStars = false;
+        //oneStars = false;
+        //twoStars = true;
+        //fiveStars = false;
         oneStarChance = 0;
         twoStarChance = 885;
         threeStarChance = 85;
@@ -226,9 +48,9 @@ submit.addEventListener("click", function() {
         alert("set to band");
     } else if (type == 3) {
         largePull = 1;
-        oneStars = true;
-        twoStars = true;
-        fiveStars = true;
+        //oneStars = true;
+        //twoStars = true;
+        //fiveStars = true;
         oneStarChance = 20;
         twoStarChance = 35;
         threeStarChance = 27;
@@ -238,9 +60,9 @@ submit.addEventListener("click", function() {
         alert("set to adopt");
     } else if (type == 4) {
         largePull = 3;
-        oneStars = true;
-        twoStars = true;
-        fiveStars = true;
+        //oneStars = true;
+        //twoStars = true;
+        //fiveStars = true;
         oneStarChance = 394;
         twoStarChance = 265;
         threeStarChance = 200;
@@ -249,7 +71,8 @@ submit.addEventListener("click", function() {
         max = 1000;
         alert("set to bubblegum");
     };
-}); //changeType
+});
+
 onePull.addEventListener("click", function() {
     //alert("1 pull…");
     onePull.style = "visibility: hidden;";
@@ -257,31 +80,80 @@ onePull.addEventListener("click", function() {
     message.style = "visibility: visible;";
     continueButton.style = "visibility: visible;";
 
+    let type = gachaType.value;
     let pull = Math.floor(Math.random() * max) + 1;
 
-    if (oneStars == true && pull <= oneStarChance) {
+    if (type == 1) {
+        if (pull <= threeStarChance) {
+            rewards.push("3");
+        };
+        if (pull > threeStarChance && pull <= threeStarChance + fourStarChance) {
+            rewards.push("4");
+        };
+        if (pull > threeStarChance + fourStarChance && pull <= threeStarChance + fourStarChance + fiveStarChance) {
+            rewards.push("5");
+        };
+    };
+    if (type == 2) {
+        if (pull <= twoStarChance) {
+            rewards.push("2");
+        };
+        if (pull > twoStarChance && pull <= twoStarChance + threeStarChance) {
+            rewards.push("3");
+        };
+        if (pull > twoStarChance + threeStarChance && pull <= twoStarChance + threeStarChance + fourStarChance) {
+            rewards.push("4");
+        };
+    };
+    if (type == 3 || type == 4) {
+        if (pull <= oneStarChance) {
+            rewards.push("1");
+        };
+        if (pull > oneStarChance && pull <= oneStarChance + twoStarChance) {
+            rewards.push("2");
+        };
+        if (pull > oneStarChance + twoStarChance && pull <= oneStarChance + twoStarChance + threeStarChance) {
+            rewards.push("3");
+        };
+        if (pull > oneStarChance + twoStarChance + threeStarChance && pull <= oneStarChance + twoStarChance + threeStarChance + fourStarChance) {
+            rewards.push("4");
+        };
+        if (pull > oneStarChance + twoStarChance + threeStarChance + fourStarChance && pull <= oneStarChance + twoStarChance + threeStarChance + fourStarChance + fiveStarChance) {
+            rewards.push("5");
+        };
+    };
+
+    /*if (oneStars == true && pull <= oneStarChance) {
         rewards.push("1");
     };
-    if (oneStars == true && twoStars == true && pull > oneStarChance && pull <= twoStarChance) {
+    if (oneStars == true && twoStars == true && pull > oneStarChance && pull <= oneStarChance + twoStarChance) {
         rewards.push("2");
     };
     if (oneStars == false && twoStars == true && pull <= twoStarChance) {
         rewards.push("2");
     };
-    if (twoStars == true && pull > twoStarChance && pull <= threeStarChance) {
+    if (oneStars == true && twoStars == true && pull > oneStarChance + twoStarChance && pull <= oneStarChance + twoStarChance + threeStarChance) {
         rewards.push("3");
     };
-    if (twoStars == false && gatchaType.value == "1" && pull <= threeStarChance) {
+    if (oneStars == false && twoStars == true && pull > twoStarChance && pull <= twoStarChance + threeStarChance) {
         rewards.push("3");
     };
-    if (pull > threeStarChance && pull <= fourStarChance) {
+    if (twoStars == true && pull <= twoStarChance + threeStarChance) {
+        rewards.push("3");
+    };
+    if (twoStars == false && pull <= threeStarChance) {
+        rewards.push("3");
+    };
+    if (pull > threeStarChance && pull <= threeStarChance + fourStarChance) {
         rewards.push("4");
     };
-    if (fiveStars == true && pull > fourStarChance && pull <= fiveStarChance) {
+    if (fiveStars == true && pull > threeStarChance + fourStarChance && pull <= threeStarChance + fourStarChance + fiveStarChance) {
         rewards.push("5");
-    };
+    };*/
+    mode = 1;
     //alert("u pulled a " + rewards[rewards.length - 1] + "-star!!");
-}); //doPull
+});
+
 largePull.addEventListener("click", function() {
     alert(largePullAmount + " pull…");
     message.style = "visibility: visible;";
@@ -290,45 +162,51 @@ largePull.addEventListener("click", function() {
 
     for (let i = 1; i <= largePullAmount; i++) {
         //alert("1 pull…");
+        let type = gachaType.value;
         let pull = Math.floor(Math.random() * max) + 1;
 
-        if (oneStars == true && pull <= oneStarChance) {
-            rewards.push("1");
-            continue;
-        };
-        if (oneStars == true && twoStars == true && pull > oneStarChance && pull <= twoStarChance) {
-            rewards.push("2");
-            continue;
-        };
-        if (oneStars == false && twoStars == true && pull <= twoStarChance) {
-            rewards.push("2");
-            continue;
-        };
-        if (twoStars == true && pull > twoStarChance && pull <= threeStarChance) {
-            rewards.push("3");
-            continue;
-        };
-        if (twoStars == false && gatchaType.value == "1" && pull <= threeStarChance) {
-            rewards.push("3");
-            continue;
-        };
-        if (pull > threeStarChance && pull <= fourStarChance) {
-            rewards.push("4");
-            if (gachaType.value == "1") {
+        if (type == 1) {
+            if (pull <= threeStarChance) {
+                rewards.push("3");
+            };
+            if (pull > threeStarChance && pull <= threeStarChance + fourStarChance) {
+                rewards.push("4");
                 if (highest < 4) {
                     highest = 4;
                 };
             };
-            continue;
-        };
-        if (fiveStars == true && pull > fourStarChance && pull <= fiveStarChance) {
-            rewards.push("5");
-            if (gachaType.value == "1") {
-                if (highest < 5) {
-                    highest = 5;
-                };
+            if (pull > threeStarChance + fourStarChance && pull <= threeStarChance + fourStarChance + fiveStarChance) {
+                rewards.push("5");
+                highest = 5;
             };
-            continue;
+        };
+        if (type == 2) {
+            if (pull <= twoStarChance) {
+                rewards.push("2");
+            };
+            if (pull > twoStarChance && pull <= twoStarChance + threeStarChance) {
+                rewards.push("3");
+            };
+            if (pull > twoStarChance + threeStarChance && pull <= twoStarChance + threeStarChance + fourStarChance) {
+                rewards.push("4");
+            };
+        };
+        if (type == 3 || type == 4) {
+            if (pull <= oneStarChance) {
+                rewards.push("1");
+            };
+            if (pull > oneStarChance && pull <= oneStarChance + twoStarChance) {
+                rewards.push("2");
+            };
+            if (pull > oneStarChance + twoStarChance && pull <= oneStarChance + twoStarChance + threeStarChance) {
+                rewards.push("3");
+            };
+            if (pull > oneStarChance + twoStarChance + threeStarChance && pull <= oneStarChance + twoStarChance + threeStarChance + fourStarChance) {
+                rewards.push("4");
+            };
+            if (pull > oneStarChance + twoStarChance + threeStarChance + fourStarChance && pull <= oneStarChance + twoStarChance + threeStarChance + fourStarChance + fiveStarChance) {
+                rewards.push("5");
+            };
         };
         //alert("u pulled a " + rewards[rewards.length - 1] + "-star!!");
     };
@@ -351,17 +229,30 @@ largePull.addEventListener("click", function() {
         message.innerHTML = "Click to hatch";
         continueButton.innerHTML = "🥚";
     };
+    mode = 1;
     //alert("completed execution");
-}); //doLargePull
+});
+
 continueButton.addEventListener("click", function() {
-    message.innerHTML = "you pulled: " + rewards.toString() + ". good job!!!";
-    onePull.style = "visibility: visible;";
-    largePull.style = "visibility: visible;";
-    message.style = "visibility: hidden;";
-    continueButton.style = "visibility: hidden;";
-    rewards.length = 0;
-    //alert("awards are displayed!");
-}); //reward
+    if (mode == 1) {
+        mode = 2;
+        message.innerHTML = "you pulled: " + rewards.toString() + ". good job!!!";
+        message.style = "visibility: visible;";
+        continueButton.innerHTML = "OK";
+        continueButton.style = "visibility: visible;";
+        onePull.style = "visibility: visible;";
+        largePull.style = "visibility: visible;";
+        rewards.length = 0;
+        //alert("awards are displayed!");
+    } else if (mode == 2) {
+        mode = 0;
+        message.innerHTML = "";
+        message.style = "visibility: hidden;";
+        continueButton.innerHTML = "";
+        continueButton.style = "visibility: hidden;";
+    };
+});
+
 colorBlindHelp.addEventListener("click", function() {
     if (colorBlindOn == true) {
         document.html.class = "colorBlind";
@@ -371,4 +262,4 @@ colorBlindHelp.addEventListener("click", function() {
         colorBlindOn = true;
     };
     //alert("set color blind mode to " + colorBlindOn);
-}); //colorBlind
+});
