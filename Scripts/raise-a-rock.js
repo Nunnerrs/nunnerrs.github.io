92ffa481d576e38b5ab25b352fe88c96bb1007d6
Nunners <97916154+Nunnerrs@users.noreply.github.com>
2023-01-24 12:10:59 -0800

    Update raise-a-rock.js
    fixed aging

--- Scripts/raise-a-rock.js
+++ Scripts/raise-a-rock.js
@@ -1,8 +1,15 @@
 var age = 1;
 const rocks {
-    good: {},
-    neutral: {},
-    bad: {}
+    good: {
+        good: [""],
+        neutral: ["Assets/Raise-A-Rock/", "", ""],
+        bad: [""],
+    },
+    bad: {
+        good: ["Assets/Raise-A-Rock/bricks.png", "Assets/Raise-A-Rock/man-rock.png", "Assets/Raise-A-Rock/expressionless.png"],
+        neutral: ["Assets/Raise-A-Rock/potato-rock.png", "", ""],
+        bad: [""],
+    };
 };
 
 const bools = [true, false, false];
@@ -43,41 +50,74 @@ var playButton = document.getElementById("playButton");
 var renameButton = document.getElementById("renameButton");
 
 function tick() {
-    if (ticking == true) {
-        age += 0.01;
-        ageDisplay.innerHTML = age;
-        
-        let hungry = bools[Math.floor(Math.random() * (bools.length - 1))];
-        if (hungry == true) {
-            full.innerHTML = Number(full.innerHTML) - 2;
-        };
-        
-        let dirty = bools[Math.floor(Math.random() * (bools.length - 1))];
-        if (dirty == true) {
-            clean.innerHTML = Number(clean.innerHTML) - 2;
-        };
-        
-        let bored = bools[Math.floor(Math.random() * (bools.length - 1))];
-        if (bored == true) {
-            bored.innerHTML = Number(bored.innerHTML) - 2;
-        };
-        
-        if (age >= 2.5 && age < 7.5) {
-            if (full >= 75 && clean >= 75 && happy >= 75) {
-                rock.src = "../Assets/Raise-A-Rock/kid-rock-happy.png";
-            } else {
-                rock.src = "../Assets/Raise-A-Rock/kid-rock-sad.png";
+    setInterval(function() {
+        if (ticking == true) {
+            age += 0.01;
+            ageDisplay.innerHTML = age;
+            
+            let hungry = bools[Math.floor(Math.random() * (bools.length - 1))];
+            if (hungry == true) {
+                full.innerHTML = Number(full.innerHTML) - 2;
             };
-        } else if (age <= 7.5) {
-            if (full >= 90 && clean >= 90 && happy >= 90) {
-                rock.src = rocks[good][Math.floor(Math.random() * (rocks[good].length - 1));
-            } else if (full >= 70 && full < 90 && clean >= 70 && clean < 90 && happy >= 70 && happy < 90) {
-                rock.src = rocks[neutral][Math.floor(Math.random() * (rocks[neutral].length - 1));
-            } else if (full < 70 && clean < 70 && happy < 70) {
-                rock.src = rocks[bad][Math.floor(Math.random() * (rocks[bad].length - 1));
+            
+            let dirty = bools[Math.floor(Math.random() * (bools.length - 1))];
+            if (dirty == true) {
+                clean.innerHTML = Number(clean.innerHTML) - 2;
+            };
+            
+            let bored = bools[Math.floor(Math.random() * (bools.length - 1))];
+            if (bored == true) {
+                bored.innerHTML = Number(bored.innerHTML) - 2;
+            };
+        
+            if (age >= 2.5 && age < 7.5) {
+                if (full >= 75 && clean >= 75 && happy >= 75) {
+                    rock.src = "Assets/Raise-A-Rock/kid-rock-happy.png";
+                } else {
+                    rock.src = "Assets/Raise-A-Rock/kid-rock-sad.png";
+                };
+            } else if (age <= 7.5) {
+                if (full >= 90 && clean >= 90 && happy >= 90) {
+                    if (rock.src == "Assets/Raise-A-Rock/kid-rock-happy.png") {
+                        rock.src = rocks["good"]["good"][Math.floor(Math.random() * (rocks["good"]["good"].length - 1));
+                    } else {
+                        rock.src = rocks["bad"]["good"][Math.floor(Math.random() * (rocks["bad"]["good"].length - 1));
+                    };
+                } else if (full >= 70 && full < 90 && clean >= 70 && clean < 90 && happy >= 70 && happy < 90) {
+                    if (rock.src == "Assets/Raise-A-Rock/kid-rock-happy.png") {
+                        rock.src = rocks["good"]["neutral"][Math.floor(Math.random() * (rocks["good"]["neutral"].length - 1));
+                    } else {
+                        rock.src = rocks["bad"]["neutral"][Math.floor(Math.random() * (rocks["bad"]["neutral"].length - 1));
+                    };
+                } else if (full < 70 && clean < 70 && happy < 70) {
+                    if (rock.src == "Assets/Raise-A-Rock/kid-rock-happy.png") {
+                        rock.src = rocks["good"]["bad"][Math.floor(Math.random() * (rocks["good"]["bad"].length - 1));
+                    } else {
+                        rock.src = rocks["bad"]["bad"][Math.floor(Math.random() * (rocks["bad"]["bad"].length - 1));
+                    };
+                };
             };
         };
+    }), 1000);
+};
+
+function override(event) {
+    event.preventDefault();
+};
+
+function drag(event) {
+    event.dataTransfer.setData("tool", event.target.id);
+};
+
+function use(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("tool");
+    if (data == "plant" || data == "variety" || data == "white") {
+        full.innerHTML = Number(full.innerHTML) + 1;
+    };
+    if (data == "shower" || data == "sponge") {
+        clean.innerHTML = Number(clean.innerHTML) + 1;
     };
 };
 
-window.onload = setInterval(tick, 1000);
+window.onload = tick;
