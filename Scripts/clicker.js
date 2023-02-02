fdb6224d75f8240f3a680b4da921e56a21fdc3f3
Nunners <97916154+Nunnerrs@users.noreply.github.com>
2023-02-02 10:28:14 -0800

    Update clicker.js
    added saving & changed importing

--- Scripts/clicker.js
+++ Scripts/clicker.js
@@ -1,4 +1,7 @@
 var clicks = 0;
+if (localStorage.getItem("clicks") != null) {
+    clicks = Number(localStorage.getItem("clicks"));
+};
 var clickCounter = document.getElementById("clicks");
 var increment = document.getElementById("increment");
 var clickButton = document.getElementById("clickButton");
@@ -8,7 +11,11 @@ var textButton = document.getElementById("textButton");
 var cpc = document.getElementById("cpc");
 var c2u = document.getElementById("c2u");
 
-function counter() {
+setInterval(function() {
+    localStorage.setItem("clicks", clicks);
+}, 100)
+
+function click() {
     clicks += Number(increment.innerHTML);
     clickCounter.innerHTML = clicks;
 }
@@ -16,27 +23,18 @@ function importClicks() {
     var cc = prompt("Enter the number of clicks to import");
     if (Math.floor(Number(cc)) > 0) {
         if (clicks != 0) {
-            let yn = prompt("Add " + Math.floor(cc) + " to " + clicks + "? (Type in \"yes\" or \"no\")");
-            if (yn.toLowerCase().indexOf("y") != -1) {
-                alert("Set clicks to " + (clicks + Math.floor(Number(cc))));
-                clicks += Math.floor(Number(cc));
-            } else {
-                alert("Set clicks to " + cc);
-                clicks = Math.floor(Number(cc));
-            }
-        } else {
-            alert("Set clicks to " + cc)
-            clicks = Math.floor(Number(cc));
-        }
-    }
+            clicks += Math.floor(Number(cc));
+            alert("Set clicks to " + (clicks + Math.floor(Number(cc))));
+        };   
+    };
     clickCounter.innerHTML = clicks;
 };
 function toggleText() {
     var button = document.getElementById("clickButton")
-    if (button.innerHTML != "Click Me!") {
-        button.innerHTML = "Click Me!"
+    if (button.innerHTML != "") {
+        button.innerHTML = "";
     } else {
-        button.innerHTML = ""
+        button.innerHTML = "Click Me!";
     }
 };
 function upgradeClick() {
@@ -45,12 +43,12 @@ function upgradeClick() {
         cpc.innerHTML = Number(cpc.innerHTML) + 1;
         c2u.innerHTML = Math.round(Number(c2u.innerHTML) + 50 * (cpc.innerHTML * (1.5 + cpc.innerHTML / 3)));
     } else {
-        let clicksLeft = Number(c2u.innerHTML) - clicks
-        alert("You don't have enough clicks to upgrade your click! (Don't worry, you're only " + clicksLeft + " clicks away!)")
+        let clicksLeft = Number(c2u.innerHTML) - clicks;
+        alert("You don't have enough clicks to upgrade your click! (You're only " + clicksLeft + " clicks away!)");
     }
 };
 
-clickButton.addEventListener("onclick", counter);
+clickButton.addEventListener("onclick", click);
 upgradeButton.addEventListener("onclick", upgradeClick);
 importButton.addEventListener("onclick", importClicks);
 textButton.addEventListener("onclick", toggleText);
